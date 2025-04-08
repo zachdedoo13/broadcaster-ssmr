04e5820b49fc8841f65899f3dce46f54137cdf71
zacha <zachary.campbell2006@outlook.com>
2025-04-08 13:16:48 +1000

    fc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! A Broadcaster (ssmr = single sender multi receiver) is a channel where one sender sends duplicate data to multiple receiver's.
 //! the data is buffered (as specified by Broadcaster::subscribe(buffer_size: usize)) at the receiver and can be pulled with 'recv' or 'try_recv'.
-//!
+//! All receivers will get sent a copy of the data, if a receivers buffer overflows it will block Broadcaster::send().
+
 
 use std::collections::VecDeque;
 use std::sync::atomic::{AtomicBool, Ordering};
